9f74c2fae3c6726db306b34b94c6f5158f920228
juniorots <juniorots@gmail.com>
2018-07-07 18:24:55 -0300

    atualizando o projeto

--- src/main/java/br/com/igetecar/controller/ClienteMB.java
+++ src/main/java/br/com/igetecar/controller/ClienteMB.java
@@ -148,7 +148,7 @@ public class ClienteMB implements Serializable {
         }
         
         ArrayList emails = new ArrayList();
-//        emails.add("marcondes@gmail.com");
+        emails.add("igetecar2@gmail.com");
         
         EnviarEmail.tratarEnvio(emails, "Notificação de usuário - IGETE CAR", getCliente().getMensagem(), getCliente().getNome(), getCliente().getEmail());
         
